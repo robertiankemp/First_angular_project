95ad244af5405e2beef9ddbb8a863586b1484677
Rob Kemp <robert.ian.kemp@gmail.com>
2014-05-28 17:26:22 +0100

    adds clickable tabs to each product

--- app.js
+++ app.js
@@ -5,27 +5,44 @@
 		this.products = gems;
 	});
 
+	app.controller('TabController', function() {
+		this.tab = 1;
+		  this.setTab = function(selectedTab){
+      this.tab = selectedTab;
+    };
+      this.isSet = function(checkTab){
+      return this.tab === checkTab; 
+      };
+	});
+
 	var gems = [
 	  {
 		  name: 'Emerald',
 			price: 1000,
-			description: ". . .",
+			description: "A lovely green emerald for all occasions",
 			canPurchase: false,
-			soldOut: false
+			soldOut: false,
+			shine: 5
 	  },
 	  {
 			name: 'Ruby',
 			price: 500,
-			description: ". . .",
+			description: "A deep red",
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			shine: 5,
+			images: [
+				"images/rubylang.png",
+				"images/ruby-on-rails-steams-critical-security-patch.jpg"
+			]
 	  },
 	  {
 			name: 'Diamond',
 			price: 2000,
-			description: ". . .",
+			description: "the daddy of the gems",
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			shine: 7
 	  }
  
 	];
